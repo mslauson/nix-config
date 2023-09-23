a3e31334c22f8019d21b47a652f8646260af8557
Matthew Slauson <matthew@slauson.io>
2023-09-22 20:17:23 -0500

    change

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 currentPath="$(pwd)"
 
-for file in configuration.nix hardware-configuration.nix; do
+for file in configuration.nix; do
 	rm -rf /etc/nixos/$file
 	sudo ln -s "$currentPath"/"$file" /etc/nixos/"$file"
 done
