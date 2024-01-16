c18c1958e4a38da78de2561dab000214f72ee437
Matthew Slauson <matthew@slauson.io>
2024-01-16 13:00:28 -0600

    fix

---
+++ config/nixpkgs/flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "Home-manager configuration";
+
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, home-manager, nixpkgs, utils }:
+    let
+      pkgsForSystem = system: import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+
+      mkHomeConfiguration = args: home-manager.lib.homeManagerConfiguration (rec {
+        modules = [ (import ./home.nix) ];
+        pkgs = pkgsForSystem (args.system or "x86_64-linux");
+      } // { inherit (args) extraSpecialArgs; });
+
+    in utils.lib.eachSystem [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ] (system: rec {
+      legacyPackages = pkgsForSystem system;
+  }) // {
+    # non-system suffixed items should go here
+    nixosModules.home = import ./home.nix; # attr set or list
+
+    homeConfigurations.popos = mkHomeConfiguration {
+      extraSpecialArgs = {
+        withGUI = false ;
+        isDesktop = false;
+        networkInterface = "enp5s0";
+      };
+    };
+    #
+    # homeConfigurations.server = mkHomeConfiguration {
+    #   extraSpecialArgs = {
+    #     withGUI = false;
+    #     isDesktop = false;
+    #     networkInterface = "enp68s0";
+    #   };
+    # };
+    #
+    # homeConfigurations.laptop = mkHomeConfiguration {
+    #   extraSpecialArgs = {
+    #     withGUI = true;
+    #     isDesktop = true;
+    #     networkInterface = "wlp1s0";
+    #   };
+    # };
+    #
+    # homeConfigurations.mac-mini = mkHomeConfiguration {
+    #   extraSpecialArgs = {
+    #     withGUI = false;
+    #     isDesktop = false;
+    #     networkInterface = "en1";
+    #   };
+    # };
+
+    inherit home-manager;
+    inherit (home-manager) packages;
+  };
+}
