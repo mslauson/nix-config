e7314edb74ba688edfabcc89a332cc732142128e
Matthew Slauson <matthew@slauson.io>
2024-01-16 12:32:35 -0600

    fix init

--- init.sh
+++ init.sh
@@ -2,7 +2,13 @@
 
 currentPath="$(pwd)"
 
-for file in configuration.nix; do
-	rm -rf /etc/nixos/$file
-	sudo ln -s "$currentPath"/"$file" /etc/nixos/"$file"
-done
+# for file in configuration.nix; do
+# 	rm -rf /etc/nixos/$file
+# 	sudo ln -s "$currentPath"/"$file" /etc/nixos/"$file"
+# done
+
+rm -rf ~/nix
+ln -s $currentPath/nix ~/nix
+
+# nix --extra-experimental-features nix-command run nixpkgs#home-manager -- switch --flake ~/nix/flakes.nix
+nix --extra-experimental-features "nix-command flakes" build ~/nix/
